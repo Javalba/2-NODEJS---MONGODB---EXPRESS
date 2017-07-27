ff899bd16af89abc955209981c036a101095e6d7
Javier Armero <Javalba90@gmail.com>
2017-07-27 18:58:14 +0200

    Step 1: Project Introduction

---
+++ ironfunding/app.js
@@ -0,0 +1,81 @@
+// app.js
+
+const express            = require('express');
+const path               = require('path');
+const favicon            = require('serve-favicon');
+const logger             = require('morgan');
+const cookieParser       = require('cookie-parser');
+const bodyParser         = require('body-parser');
+const expressLayouts     = require('express-ejs-layouts');
+const mongoose           = require('mongoose');
+
+//Authentication & Authorization modules
+const passport           = require('passport');
+const session            = require('express-session');
+const MongoStore         = require('connect-mongo')(session);
+
+//MongoDB connection. Database: ironfunds-development
+mongoose.connect('mongodb://localhost:27017/ironfunds-development');
+
+
+const app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('layout', 'layouts/main');
+app.use(expressLayouts);
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components/')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.js
+app.use(session({
+  secret: 'ironfundingdev',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+app.use( (req, res, next) => {
+  if (typeof(req.user) !== "undefined"){
+    res.locals.userSignedIn = true;
+  } else {
+    res.locals.userSignedIn = false;
+  }
+  next();
+});
+
+const index = require('./routes/index');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
